053d9a5f4b1716fa54c74661962942a5408dfd62
Dedlipid <bamshamaei@gmail.com>
2016-05-17 16:51:58 -0400

    Now the size of the sides can be determined manually.

--- shape.py
+++ shape.py
@@ -3,11 +3,20 @@ import turtle
 import random
 t = turtle.Pen()
 def make_shape():
-	sides = input("How many sides mister?");sides = int(sides);size = math.sqrt(sides)*16;t.left(90);t.forward(size*3);t.right(90);t.clear();t.left(180);
-	user_color = input("What color do you want mister?")
+	sides = input("How many sides? ")
+	sides = int(sides)
+	size = input("How long should each side be?(In Pixels) ")
+	size = int(size)
+	t.forward(size*3)
+	t.right(90)
+	t.clear()
+	t.left(180)
+	user_color = input("What color do you want mister? ")
 	t.color(user_color)
 	t.begin_fill()
 	for x in range(sides):
-		t.forward(size);t.left(float((180-(((sides-2)*180.0)/sides))));
-	t.end_fill();t.right(180-(sides*270));
+		t.forward(size)
+		t.left(180-(((sides-2)*180.0)/sides))
+	t.end_fill()
+	t.right(sides*270.0)
 make_shape()	
