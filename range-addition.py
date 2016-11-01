77d96656b6a90fa6b4a51c1e6d07f41b91cf6bb8
Dedlipid <bamshamaei@gmail.com>
2016-11-01 16:45:20 -0300

    Update range-addition.py

--- range-addition.py
+++ range-addition.py
@@ -1,20 +1,31 @@
 import math
 import time
 def give_sumof_range():
+    try:
 
-    n = raw_input('Two numbers please (with a comma in between): ')
-    n=n.split(",")
+        n = raw_input('Two numbers please (with a comma in between): ')
+        n=n.split(",")
 
-    if n[0]>n[1]:
-        i = int(n[0])
-        j = int(n[1])
-    else:
-        i = int(n[1])
-        j = int(n[0])
-    result = (i*(i+1)) - (j*(j-1))
-    print result/2
+        if n[0]>n[1]:
+            i = int(n[0])
+            j = int(n[1])
+        else:
+            i = int(n[1])
+            j = int(n[0])
+
+        result = (i*(i+1)) - (j*(j-1))
+        print result/2
+
+        give_sumof_range()
+    except (IndexError,ValueError,ValueError):
+        give_sumof_range()
+
+        
 give_sumof_range()
-time.sleep(100)
+
+
+
+    
 
 
     
