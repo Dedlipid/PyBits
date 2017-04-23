e8c08288f36288ba17bcdd9d4d20fed09d67c8c1
Dedlipid <bamshamaei@gmail.com>
2017-04-22 20:22:57 -0400

    Add files via upload

---
+++ 456.py
@@ -0,0 +1,35 @@
+def f(num):
+    steps = 0                                  
+    if num == 1:
+        print "steps = 0"
+    if num == 0:
+        print "Steps are infinite"
+    if num >1:
+        while num>1:
+            if num % 2 ==0:
+                num = num/2
+                ##print "the number is",num
+                steps = steps +1
+                ##print "The steps are",steps
+            else :
+                num = num + 1
+                ##print "The number is",num
+                steps = steps +1
+                ##print "The steps are",steps
+        print "it took",steps,"Steps" 
+
+def listmaker():
+    list_begin=int(raw_input("From what number should the list start? "))
+    list_end=int(raw_input("What should be the last item in the list? "))
+    n = int(raw_input("at what interval?"))
+    
+    for x in range(list_begin,list_end+1):
+        if x %n == 0:
+            print "------------------THE NUMBER IS ",x,"------------------"
+            f(x)
+    get = raw_input("Run again?(y/n)")
+    if get == "y":
+        listmaker()
+listmaker()
+                    
+
