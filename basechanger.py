053fe4610685aaa93e9343e193ceb7eebca51faf
Dedlipid <bamshamaei@gmail.com>
2017-04-30 21:09:37 -0400

    Update basechanger.py

--- basechanger.py
+++ basechanger.py
@@ -6,15 +6,18 @@ def f():
   l=[]
   if n == 0 :
     print "0 in any base is 0"
-  else:  
-    while n >0:
+  else:
+    while n > 0:
       l.extend([n % b])
-      n  = (n - (n % b))/b
-    l = l[::-1]
-    de = ''.join(str(e) for e in l)
-    de = int(de)
-    print de
+      n  = n/b
+  l = l[::-1]
+  if b <=10:
+      l = ''.join(str(e) for e in l)
+  else :
+      l = ' '.join(str(e) for e in l)
+  print l
   c = raw_input("Press enter to run again")
   if c == "":
     f()
 f()
+
