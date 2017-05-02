a9f6860a829de8f05d886ff4567b4c5378706298
Dedlipid <bamshamaei@gmail.com>
2017-05-01 23:56:25 -0400

    Update 456.py

--- 456.py
+++ 456.py
@@ -1,30 +1,25 @@
 def f(num):
     steps = 0                                  
-    if num == 1:
-        print "steps = 0"
-    if num == 0:
-        print "Steps are infinite"
-    if num >1:
-        while num>1:
-            if num % 2 ==0:
-                num = num/2
-                steps = steps +1
-            else :
-                num = num + 1
-                steps = steps +1
-        print "it took",steps,"Steps" 
+    while num>1:
+        if num % 2 == 0:
+            num /= 2
+            steps += 1
+        else :
+            num += 1
+            steps += 1
+    print "It takes",steps,"steps"
+
 def listmaker():
-    list_begin=int(raw_input("From what number should the list start? "))
-    list_end=int(raw_input("What should be the last item in the list? "))
-    n = int(raw_input("at what interval?"))
-    
+    list_begin=int(raw_input("From what number should the list start? \n"))
+    list_end=int(raw_input("What should be the last item in the list? \n"))
+    n = int(raw_input("What number should they be a multiple of? \n"))
     for x in range(list_begin,list_end+1):
-        if x %n == 0:
-            print "------------------THE NUMBER IS ",x,"------------------"
+        if x % n == 0:
+            print "------------------","THE NUMBER IS ",x,"------------------"
             f(x)
-    get = raw_input("Run again?(y/n)")
+    get = raw_input("Run again?(y/n) \n")
     if get == "y":
         listmaker()
+
 listmaker()
-                    
 
